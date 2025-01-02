6f5c208b7f695c292943d43f77339f55bb4e4f1b
raulmarquez93 <raumarroi@alu.edu.gva.es>
2025-01-02 20:04:47 +0100

    update csv

--- frontend/script.js
+++ frontend/script.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     language: language   // Idioma
                 })
             });
+            
 
             const data = await response.json();
 
